669030b00a4e9982fa2df9ed4de0f04476a7a42a
Joseph <josephkim828@gmail.com>
2021-12-06 17:26:11 +0900

    Feat: added rest api script

---
+++ start.sh
@@ -0,0 +1,6 @@
+curl \
+  --header "Content-type: application/json" \
+  --header "apiKey: ca03na188ame03u1d78620de67282882a84" \
+  --request POST \
+  --data '{"model": 1, "text": "test", "reusable": true}' \
+  http://localhost:3000/voice
